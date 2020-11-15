e21d4ea9ba19ed2dad9a14c45c78229b2ef6a121
Sebastian <sebastol90@wp.pl>
2020-11-15 13:42:25 +0100

    Apply react router

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import GlobalStyles from '../theme/globalStyles';
 import theme from '../theme/theme';
 
 const Root = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
+    <Router>
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/signin" />
+        <Route path="/addclassroom" />
+        <Route path="/adduser" />
+        <Route path="/findclassroom" />
+        <Route path="/classroomdesc/:id" />
+        <Route path="/classroomdesc/:id/confirm" />
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
