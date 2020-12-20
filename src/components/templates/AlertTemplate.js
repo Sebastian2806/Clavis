d24540d4bf2b52b771b8e06d93d48a066275bfc7
Sebastian <sebastol90@wp.pl>
2020-12-20 23:32:13 +0100

    Create alertdialog

--- src/components/templates/AlertTemplate.js
+++ src/components/templates/AlertTemplate.js
@@ -1,20 +1,100 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Button from '../atoms/Button';
 
-const StyledWrapper = styled.div``;
+const StyledWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100000;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.5);
+  align-items: center;
+  justify-content: center;
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+`;
 
-const AlertTemplate = ({ children, isOpen, type }) => {
+const StyledAlert = styled.div`
+  width: fit-content;
+  width: 100%;
+  max-width: 400px;
+  display: grid;
+  grid-template-rows: 60px 1fr;
+  margin: 15px;
+  border-radius: ${({ theme }) => theme.radius};
+  overflow: hidden;
+`;
+
+const StyledHeader = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 50px;
+  gap: 10px;
+  padding: 0 10px;
+  background-color: ${({ theme }) => theme.colors.dark};
+  color: ${({ theme }) => theme.colors.light};
+`;
+
+const StyledTitle = styled.h2`
+  text-transform: capitalize;
+`;
+
+const StyledContent = styled.div`
+  padding: 10px;
+  background-color: ${({ theme }) => theme.colors.light};
+`;
+
+const StyledDesc = styled.p`
+  font-size: 20px;
+  margin-bottom: 20px;
+`;
+
+const CancelButton = styled(Button).attrs({
+  'aria-label': 'Anuluj akcje',
+})`
+  color: ${({ theme }) => theme.colors.dark};
+  background-color: ${({ theme }) => theme.colors.light};
+  border: 3px solid ${({ theme }) => theme.colors.dark};
+`;
+
+const AlertTemplate = ({ children, isOpen, type, title, desc }) => {
+  const [btns, setBtns] = useState([]);
+  const btnsContainer = useRef();
+
+  useEffect(() => {
+    if (btnsContainer.current) {
+      setBtns(btnsContainer.current.children);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isOpen) {
+      btns[0].focus();
+    }
+  }, [isOpen]);
+
   return (
-    <div>
-      <div>{children}</div>
-    </div>
+    <StyledWrapper isOpen={isOpen} role={type} aria-modal="true" aria-labelledby="title">
+      <StyledAlert>
+        <StyledHeader>
+          <StyledTitle id="title">{title}</StyledTitle>
+        </StyledHeader>
+        <StyledContent>
+          <StyledDesc id="desc">{desc}</StyledDesc>
+          {children(CancelButton, btnsContainer)}
+        </StyledContent>
+      </StyledAlert>
+    </StyledWrapper>
   );
 };
 
 AlertTemplate.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
   type: PropTypes.string,
 };
 
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { Form, Formik } from 'formik';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -8,6 +8,8 @@ import Loader from '../components/molecules/Loader';
 import Checkbox from '../components/molecules/Checkbox';
 import IconBox from '../components/atoms/IconBox';
 import { users } from '../data';
+import AlertTemplate from '../components/templates/AlertTemplate';
+import Button from '../components/atoms/Button';
 
 const StyledContainer = styled.div`
   width: 100%;
@@ -54,15 +56,15 @@ const StyledUserBar = styled(StyledGridHeader)`
     background-color: ${({ theme }) => theme.colors.lightgrey};
   }
 
-  &:hover .index-number {
+  /* &:hover .index-number {
     display: none;
   }
 
   &:hover .checkbox-container {
     display: block;
-  }
+  } */
 
-  ${({ isSelected }) =>
+  /* ${({ isSelected }) =>
     isSelected &&
     css`
       & .index-number {
@@ -72,7 +74,7 @@ const StyledUserBar = styled(StyledGridHeader)`
       & .checkbox-container {
         display: block;
       }
-    `}
+    `} */
 `;
 
 const StyledUserBarElement = styled.div`
@@ -92,74 +94,130 @@ const StyledForm = styled(Form)`
   width: 100%;
 `;
 
+const StyledBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, min-content);
+  justify-content: center;
+  gap: 10px;
+`;
+
 const UserList = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState({ name: '', surname: '' });
+  const firstEL = useRef();
+
+  const handleDelete = (is, name = '', surname = '') => {
+    setIsOpen(is);
+    setCurrentUser({ name, surname });
+  };
+
+  useEffect(() => {
+    if (firstEL && isOpen === false) firstEL.current.focus();
+  }, [firstEL, isOpen]);
 
   return (
     <ViewWrapper>
       {isLoading ? (
         <Loader size={20} margin={4} />
       ) : (
-        <Formik
-          initialValues={{
-            users_id: [],
-          }}
-        >
-          {({ values, handleChange }) => (
-            <StyledForm>
-              <StyledContainer>
-                <div>
-                  <StyledHeader>
-                    <ViewTitle>Lista użytkowników</ViewTitle>
-                  </StyledHeader>
-                  <StyledContent>
-                    <StyledGridHeader>
-                      <StyledIndex>
-                        <p>#</p>
-                      </StyledIndex>
-                      <StyledUserBarElement>
-                        <p>Imię</p>
-                      </StyledUserBarElement>
-                      <StyledUserBarElement>
-                        <p>Nazwisko</p>
-                      </StyledUserBarElement>
-                      <StyledUserBarElement>
-                        <p>Usuń</p>
-                      </StyledUserBarElement>
-                    </StyledGridHeader>
-                    {users.map((user, i) => (
-                      <StyledUserBar key={user._id} isSelected={values.users_id.includes(user._id)}>
-                        <StyledIndex>
-                          <p className="index-number">{i + 1}</p>
-                          <StyledCheckboxContainer className="checkbox-container">
-                            <Checkbox
-                              type="checkbox"
-                              value={user._id}
-                              name="users_id"
-                              isListItem="false"
-                              onChange={handleChange}
-                            />
-                          </StyledCheckboxContainer>
+        <>
+          <Formik
+            initialValues={{
+              users_id: [],
+            }}
+          >
+            {({ values, handleChange }) => (
+              <StyledForm>
+                <StyledContainer>
+                  <div>
+                    <StyledHeader>
+                      <ViewTitle id="user_list_title">Lista użytkowników</ViewTitle>
+                    </StyledHeader>
+                    <StyledContent
+                      role="grid"
+                      aria-colcount="4"
+                      aria-rowcount={users.length}
+                      aria-labelledby="user_list_title"
+                    >
+                      <StyledGridHeader role="row" aria-rowindex="1">
+                        <StyledIndex role="columnheader" aria-colindex="1" aria-sort="none">
+                          <p>#</p>
                         </StyledIndex>
-                        <StyledUserBarElement>
-                          <p>{user.name}</p>
+                        <StyledUserBarElement role="columnheader" aria-colindex="2" aria-sort="none">
+                          <p>Imię</p>
                         </StyledUserBarElement>
-                        <StyledUserBarElement>
-                          <p>{user.surname}</p>
+                        <StyledUserBarElement role="columnheader" aria-colindex="3" aria-sort="none">
+                          <p>Nazwisko</p>
                         </StyledUserBarElement>
-                        <StyledUserBarElement>
-                          <IconBox type="button" mode="dark">
-                            <DeleteIcon />
-                          </IconBox>
+                        <StyledUserBarElement role="columnheader" aria-colindex="4" aria-sort="none">
+                          <p>Usuń</p>
                         </StyledUserBarElement>
-                      </StyledUserBar>
-                    ))}
-                  </StyledContent>
-                </div>
-              </StyledContainer>
-            </StyledForm>
-          )}
-        </Formik>
+                      </StyledGridHeader>
+                      {users.map((user, i) => (
+                        <StyledUserBar
+                          key={user._id}
+                          isSelected={values.users_id.includes(user._id)}
+                          role="row"
+                          aria-rowindex={2 + i}
+                        >
+                          <StyledIndex role="gridcell" aria-colindex="1">
+                            <p className="index-number">{i + 1}</p>
+                            <StyledCheckboxContainer className="checkbox-container">
+                              <Checkbox
+                                type="checkbox"
+                                value={user._id}
+                                name="users_id"
+                                isListItem={false}
+                                onChange={handleChange}
+                              />
+                            </StyledCheckboxContainer>
+                          </StyledIndex>
+                          <StyledUserBarElement role="gridcell" aria-colindex="2">
+                            <p>{user.name}</p>
+                          </StyledUserBarElement>
+                          <StyledUserBarElement role="gridcell" aria-colindex="3">
+                            <p>{user.surname}</p>
+                          </StyledUserBarElement>
+                          <StyledUserBarElement role="gridcell" aria-colindex="4">
+                            <IconBox
+                              type="button"
+                              mode="dark"
+                              aria-label={`Usuń użytkownika ${user.name} ${user.surname}`}
+                              title={`Usuń użytkownika ${user.name} ${user.surname}`}
+                              onClick={() => handleDelete(true, user.name, user.surname)}
+                              ref={i === 0 ? firstEL : null}
+                            >
+                              <DeleteIcon />
+                            </IconBox>
+                          </StyledUserBarElement>
+                        </StyledUserBar>
+                      ))}
+                    </StyledContent>
+                  </div>
+                </StyledContainer>
+              </StyledForm>
+            )}
+          </Formik>
+          <AlertTemplate
+            isOpen={isOpen}
+            title="potwierdzenie"
+            desc={`Czy na pewno chcesz usunąć użytkownika ${currentUser.name} ${currentUser.surname}?`}
+          >
+            {(CancelButton, btnContainer) => (
+              <StyledBox ref={btnContainer}>
+                <CancelButton onClick={() => handleDelete(false)}>Anuluj</CancelButton>
+                <Button
+                  aria-label={`Usuń użytkownika ${currentUser.name} ${currentUser.surname}`}
+                  title={`Usuń użytkownika ${currentUser.name} ${currentUser.surname}`}
+                  cancel
+                >
+                  Usuń
+                </Button>
+              </StyledBox>
+            )}
+          </AlertTemplate>
+        </>
       )}
     </ViewWrapper>
   );
